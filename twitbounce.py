c471deb5233d8d23389a0dfff81cc31364e575a6
netcrusher88 <netcrusher88@gmail.com>
2009-01-26 19:42:02 -0800

    added option to disable identification of poster

--- twitbounce.py
+++ twitbounce.py
@@ -3,7 +3,11 @@ import twitter
 import secret
 api = twitter.Api(username=secret.login,password=secret.password)
 dm = api.GetDirectMessages()
+id=true
 
 for message in dm:
-	api.PostUpdate(message.text + " -from " + message.sender_screen_name)
+	newpost = message.text
+	if (id):
+		newpost = newpost + " -from " + message.sender_screen_name
+	api.PostUpdate(newpost)
 	api.DestroyDirectMessage(message.id)
